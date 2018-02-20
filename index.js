50f9022ad87ef1f5799690894f6c8201d1a6d258
Laura Campbell <campbell.laura.elizabeth@gmail.com>
2018-02-20 22:57:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ var recipes = {};
 function updateObjectWithKeyAndValue (object, key, value) {
   return Object.assign ( {}, object, {[key]: value});
 }
-dest
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  
+}
